f9aacb0985220378663a89d29f1c0261efd2c921
eze-lover <cruzzzlucio@gmail.com>
2026-05-02 00:26:52 -0300

    Refactor heart creation and touch event handling

--- script.js
+++ script.js
@@ -1,93 +1,36 @@
 /* CORAZONES */
 setInterval(() => {
-  let heart = document.createElement("div");
-  heart.className = "heart";
-  heart.innerText = "💖";
+  let h = document.createElement("div");
+  h.className = "heart";
+  h.innerText = "💖";
 
-  heart.style.left = Math.random() * 100 + "vw";
-  heart.style.fontSize = (Math.random() * 20 + 10) + "px";
-  heart.style.animationDuration = (Math.random() * 3 + 2) + "s";
+  h.style.left = Math.random() * 100 + "vw";
+  h.style.fontSize = (Math.random() * 20 + 10) + "px";
+  h.style.animationDuration = (Math.random() * 3 + 2) + "s";
 
-  document.querySelector(".hearts").appendChild(heart);
+  document.body.appendChild(h);
 
-  setTimeout(() => heart.remove(), 5000);
+  setTimeout(() => h.remove(), 5000);
 }, 200);
 
-
-/* ARRASTRAR LLAVE (TACTIL) */
+/* ABRIR LIBRO */
 let llave = document.getElementById("llave");
-let candado = document.getElementById("candado");
 
-llave.addEventListener("touchmove", (e) => {
-  let touch = e.touches[0];
-  llave.style.left = touch.clientX - 20 + "px";
-  llave.style.top = touch.clientY - 20 + "px";
-});
-
-llave.addEventListener("touchend", () => {
-  let c = candado.getBoundingClientRect();
-  let l = llave.getBoundingClientRect();
-
-  if (
-    l.left < c.right &&
-    l.right > c.left &&
-    l.top < c.bottom &&
-    l.bottom > c.top
-  ) {
-    abrirLibro();
-  }
-});
-
-/* ABRIR */
-function abrirLibro() {
+llave.addEventListener("click", () => {
   document.getElementById("inicio").style.display = "none";
   document.getElementById("app").classList.remove("hidden");
 
   let musica = document.getElementById("musica");
   musica.play().catch(()=>{});
-}
-
-/* SWIPE PAGINAS */
-let paginas = document.querySelectorAll(".pagina");
-let index = 0;
-
-let startX = 0;
-
-document.addEventListener("touchstart", e => {
-  startX = e.touches[0].clientX;
 });
 
-document.addEventListener("touchend", e => {
-  let endX = e.changedTouches[0].clientX;
-
-  if (startX > endX + 50) next();
-  if (startX < endX - 50) prev();
-});
-
-function show(i) {
-  paginas.forEach(p => p.classList.remove("active"));
-  paginas[i].classList.add("active");
-}
-
-function next() {
-  if (index < paginas.length - 1) {
-    index++;
-    show(index);
-  }
-}
-
-function prev() {
-  if (index > 0) {
-    index--;
-    show(index);
-  }
-      }
+/* PASAR PAGINAS */
 let paginas = document.querySelectorAll(".page");
-let actual = 0;
+let i = 0;
 
 document.addEventListener("click", () => {
-  if (actual < paginas.length) {
-    paginas[actual].classList.add("flipped");
-    actual++;
+  if (i < paginas.length) {
+    paginas[i].classList.add("flipped");
+    i++;
   }
 });
